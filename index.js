b294cd440c080249c49a5216d4650c3dddc78131
LassassinX <sanjid8426@gmail.com>
2023-01-04 20:07:46 +0600

    updated CORS

--- index.js
+++ index.js
@@ -19,12 +19,12 @@ const total = 25;
 
 
 
-let corsOptions = {
-	origin: 'https://sanjidchowdhury.github.io', 
-	optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-  }
+// let corsOptions = {
+// 	origin: 'https://sanjidchowdhury.github.io', 
+// 	optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+//   }
 
-app.use(cors(corsOptions))
+app.use(cors())
 
 app.use(bodyParser.json())
 
